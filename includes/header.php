8114426111028488ffdbe0cff3446487966f43a1
unknown <nicolasmendes512@gmail.com>
2025-08-13 16:51:24 -0300

    Criação da Navbar na Headder e estilização no style.css

--- includes/header.php
+++ includes/header.php
@@ -14,42 +14,48 @@
 
 
 <body class="bg-dark-subtle">
+  
   <div class="container">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+
+    <!-- barra de navegação -->
+    <nav class="navbar primary-color fixed-top navbar-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          <img src="Images\logo.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+        <a class="navbar-brand fs-3 fw-semibold" href="">
+          <img src="Images\logo.png" alt="Logo" width="40" height="40" class="d-inline-block align-text-top">
           Leitura em Dia
         </a>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li>
-                  <hr class="dropdown-divider">
-                </li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-            </li>
-          </ul>
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
+        
+        <button class="navbar-toggler  align-self-stretch border-0 rounded-0 px-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <i class="bi bi-person-circle fs-3"></i>
+        </button>
+
+        <div class="offcanvas offcanvas-end secondary-color fw-medium" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title fs-3" id="offcanvasNavbarLabel">Usuário</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li>
+                    <hr class="dropdown-divider">
+                  </li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
